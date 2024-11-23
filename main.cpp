d1d068a5bc6ff8aff4aebbffdd2d2ca07f328ca5
Alexander Beliaev <beliaevs@gmail.com>
2024-11-24 02:18:35 +0300

    Alexandrescu's sample of Greedy and Fairy matching

--- main.cpp
+++ main.cpp
@@ -11,8 +11,44 @@ void printSpaced(const First& arg, const Types&... args)
 	std::cout << "\n";
 }
 
+template<typename T1, typename T2>
+void show(T1 x1, T2 x2)
+{
+	std::cout << typeid(x1).name() << " " << typeid(x2).name() << "\n";
+}
+
+// print type
+template<typename T>
+void printType(T)
+{
+	std::cout << typeid(T).name() << " ";
+}
+
+// Alexandrescu's strange example
+template<typename... Ts, typename... Us>
+void strange(Ts... ts, Us... us)
+{
+	std::cout << "Ts: ";
+	(..., printType(ts));
+	std::cout << "\n";
+	std::cout << "Us: ";
+	(..., printType(us));
+	std::cout << "\n";
+}
+
 int main()
 {
 	printSpaced(12, 3.456, "sasha");
+
+	{
+		// Alexandrescu, partial spec
+		show(2, 3.0);
+		show<double>(5, "a");
+	}
+	{
+		// Alexandrescu strange sample
+		strange(1, 2., 3.f);
+		strange<int, char>(0, 0, "aa", 3.5);
+	}
 	return 0;
 }
